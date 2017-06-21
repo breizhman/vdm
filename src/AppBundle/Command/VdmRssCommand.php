52103ed8b6450bd0e1e4c15114247835d0ae0438
Sylvain Lacot <sylvain.lacot@gmail.com>
2017-06-21 08:42:21 +0200

    Ajout de la commande de chargement du flux RSS VDM

---
+++ src/AppBundle/Command/VdmRssCommand.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace AppBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class VdmRssCommand extends ContainerAwareCommand
+{
+    protected function configure()
+    {
+        $this
+            ->setName('vdm:rss:load')
+            ->setDescription('Reads and persists the data of the RSS feed of the site VDM')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+    }
+
+}
