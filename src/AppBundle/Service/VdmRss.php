dc02289b0cc8c11dd33c8a134197ea6fc4eeda80
Sylvain Lacot <sylvain.lacot@gmail.com>
2017-06-21 19:26:22 +0200

    Load RSS a l'aide d'une limite

--- src/AppBundle/Service/VdmRss.php
+++ src/AppBundle/Service/VdmRss.php
@@ -12,33 +12,91 @@ class VdmRss
     private $feedio;
     private $entityManager;
     private $logger;
+    private $default_nb_post;
 
-    public function __construct($rssUrl, FeedIo $feedio, EntityManager  $entityManager, LoggerInterface $logger)
-    {
+    private $posts;
+
+    public function __construct(FeedIo $feedio, EntityManager  $entityManager, LoggerInterface $logger, $rssUrl, $default_nb_post)
+    {   
+        $this->posts = array();
+        $this->default_nb_post    = $default_nb_post;
         $this->rssUrl    = $rssUrl;
         $this->feedio    = $feedio;
         $this->entityManager    = $entityManager;
         $this->logger =  $logger;
     }
 
-    public function load()
+    public function load($saveToBdd = true)
     {
+        $this->loadPosts();
+
+        if($saveToBdd) {
+            $this->savePosts();
+        }
+    }
+
+    private function loadPosts($nbPost = null) {
+
+        if(is_null($nbPost)) {
+            $nbPost = $this->default_nb_post;
+        }
+
+        $total = 0;
+        $index_page = 1;
+
         $feed = $this->feedio->read($this->rssUrl)->getFeed();
+
+        do {
+
+            $total+= $this->loadPostsByPage($index_page);
+            $index_page++;
+
+        } while($total < $nbPost);
+
+        $this->posts = array_slice($this->posts,0,$nbPost);
+    }
+
+    private function loadPostsByPage($page = null)
+    {
+        $cpt = 0;
+        $url = $this->rssUrl;
+        if(is_integer($page)) {
+            $url.= '?page='.intval($page);
+        }
+
+        $feed = $this->feedio->read($url)->getFeed();
 
         foreach ( $feed as $item ) {
 
-            $post = $this->entityManager->getRepository('AppBundle:Post')->findOneByPublicId($item->getPublicId());
+            $this->posts[] = array(
+                'publicId'  => $item->getPublicId(),
+                'content'   => strip_tags($item->getDescription()),
+                'date'      => $item->getLastModified(),
+                'author'    => $item->getAuthor()->getName()
+            );
 
-            if(!$post) {
-                $post = new Post();
-                $post->setPublicId($item->getPublicId());
+            $cpt++;
+        }
+
+        return $cpt;
+    }
+
+    private function savePosts()
+    {
+        foreach($this->posts as $post)
+        {
+            $entity_post = $this->entityManager->getRepository('AppBundle:Post')->findOneByPublicId($post['publicId']);
+
+            if(!$entity_post) {
+                $entity_post = new Post();
+                $entity_post->setPublicId($post['publicId']);
             }
 
-            $post->setContent(strip_tags($item->getDescription()));
-            $post->setDate($item->getLastModified());
-            $post->setAuthor($item->getAuthor()->getName());
+            $entity_post->setContent($post['content']);
+            $entity_post->setDate($post['date']);
+            $entity_post->setAuthor($post['author']);
 
-            $this->entityManager->persist($post);
+            $this->entityManager->persist($entity_post);
         }
 
         $this->entityManager->flush();
