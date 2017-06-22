625d579169dd45d5e5c543ae918b267a7d4ed890
Sylvain Lacot <sylvain.lacot@gmail.com>
2017-06-22 08:48:59 +0200

    Ajout du bundle FOSRestBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,6 +16,8 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Debril\RssAtomBundle\DebrilRssAtomBundle(),
+            new FOS\RestBundle\FOSRestBundle(),
+            new JMS\SerializerBundle\JMSSerializerBundle(),
             new AppBundle\AppBundle(),
         ];
 
